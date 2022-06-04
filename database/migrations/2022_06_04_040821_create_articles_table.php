77edcd094b74b4d62c79ca2debb8c1f8d41851db
Andra Hafiz HSB <andra18ti@mahasiswa.pcr.ac.id>
2022-06-04 11:21:52 +0700

    Update 2022_06_04_040821_create_articles_table.php

--- database/migrations/2022_06_04_040821_create_articles_table.php
+++ database/migrations/2022_06_04_040821_create_articles_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->string('image');
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('category_id')->constrained('categories');
             $table->timestamps();
         });
     }
