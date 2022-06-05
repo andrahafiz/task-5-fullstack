96518bdff7f527879ac96c233e760c558d1c2ad8
Andra Hafiz HSB <andra18ti@mahasiswa.pcr.ac.id>
2022-06-05 11:15:20 +0700

    Update Categorie.php

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Categorie extends Model
 {
     use HasFactory;
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name',
+        'user_id',
+    ];
 }
